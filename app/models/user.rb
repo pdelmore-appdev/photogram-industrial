8b49496572e2e266979449d815b1709d679b0d3b
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-04-14 16:01:39 +0000

    add sample_data

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
 
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
   has_many :comments, foreign_key: :author_id
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
