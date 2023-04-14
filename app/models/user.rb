72f5ea41cf5db78b081efedc58c84d138bfd0bd1
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-04-14 14:48:02 +0000

    add scopes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,9 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
+
+scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+scope :by_likes, -> { order(likes_count: :desc) }
+
 end
