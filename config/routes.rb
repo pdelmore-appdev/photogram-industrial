2b33fe3c9afad134069ec66d08899f7411753b5d
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-04-17 21:13:22 +0000

    done with photos/liked photos tabs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ devise_for :users
   resources :comments
   resources :photos
 
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :feed
+  get ":username/followers" => "photos#following", as: :following
+
+  get ":username" => "users#show", as: :user
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
