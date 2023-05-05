0d7c8a2988aec4c6e061c02683f3d4ff70328be2
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-05-05 03:54:49 +0000

    navbar, feed and discover tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show liked feed discover ]
+
+
   def show
-    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def discover
+  end
+
+  def liked
+  end
+
+  def feed
+  end
+
+
+
+  def set_user
+    if params[:username]
+      @user = User.find_by!(username: params.fetch(:username))
+    else
+      @user = current_user
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root "photos#index"
+root "users#show"
 
 
 devise_for :users
@@ -10,9 +10,10 @@ devise_for :users
   resources :photos
 
 
-  get ":username/liked" => "photos#liked", as: :liked_photos
-  get ":username/feed" => "photos#feed", as: :feed
+  get ":username/liked" => "users#liked", as: :liked_photos
+  get ":username/feed" => "users#feed", as: :feed
   get ":username/followers" => "photos#following", as: :following
+  get ":username/discover" => "users#discover", as: :discover
 
   get ":username" => "users#show", as: :user
 
